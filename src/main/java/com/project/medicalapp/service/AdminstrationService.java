e456a5587606d30d44dbc043899d78f6acb1ef8e
Nicat-dev <nijatmehdiyevv@gmail.com>
2024-03-03 19:22:22 +0100

    Update Adminstation Service

--- src/main/java/com/project/medicalapp/service/AdminstrationService.java
+++ src/main/java/com/project/medicalapp/service/AdminstrationService.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 public interface AdminstrationService {
 
+    AdminstrationDto save(EmployeRegister register);
     List<AdminstrationDto> getList();
     AdminstrationDto getById(Long id);
     AdminstrationDto updateById(EmployeRegister register);
--- src/main/java/com/project/medicalapp/service/impl/AdminstrationServiceImpl.java
+++ src/main/java/com/project/medicalapp/service/impl/AdminstrationServiceImpl.java
@@ -24,6 +24,11 @@ public class AdminstrationServiceImpl implements AdminstrationService {
     private final RoleRepository roleRepository;
 
     @Override
+    public AdminstrationDto save(EmployeRegister register) {
+       return saveInfo(register);
+    }
+
+    @Override
     public List<AdminstrationDto> getList() {
         return mapper.entityListToDtoList(repository.findAll());
     }
@@ -38,10 +43,7 @@ public class AdminstrationServiceImpl implements AdminstrationService {
     @Override
     public AdminstrationDto updateById(EmployeRegister register) {
         idNullCheck(register.id());
-        Adminstration adminstration = mapper.requestToEntity(register);
-        adminstration.setRole(roleRepository.findById(register.roleId())
-                .orElseThrow(()-> new ResourceNotFoundException("Role",register.roleId().toString(),register)));
-        return mapper.entityToDto(repository.save(adminstration));
+        return saveInfo(register);
     }
 
     @Override
@@ -52,8 +54,15 @@ public class AdminstrationServiceImpl implements AdminstrationService {
 
     private void idNullCheck(Long id){
         if (Objects.isNull(id)){
-            throw new ResourceIdCanNotBeNull("id",id.toString(),id);
+            throw new ResourceIdCanNotBeNull("id","id",id);
         }
+    }
+
+    private AdminstrationDto saveInfo(EmployeRegister register){
+        Adminstration adminstration = mapper.requestToEntity(register);
+        adminstration.setRole(roleRepository.findById(register.roleId())
+                .orElseThrow(()-> new ResourceNotFoundException("Role",register.roleId().toString(),register)));
+        return mapper.entityToDto(repository.save(adminstration));
     }
 
 }
